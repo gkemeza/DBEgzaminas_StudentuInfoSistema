5467f35b8fae728499badd8d4be461c6261264bd
Gaudrius <gaudriusk@gmail.com>
2024-09-18 22:35:48 +0300

    Added DepartmentRepository.

--- DBEgzaminas_StudentuInfoSistema/Database/Repositories/DepartmentRepository.cs
+++ DBEgzaminas_StudentuInfoSistema/Database/Repositories/DepartmentRepository.cs
@@ -1,7 +1,59 @@
-﻿namespace DBEgzaminas_StudentuInfoSistema.Database.Repositories
+﻿using DBEgzaminas_StudentuInfoSistema.Database.Entities;
+using DBEgzaminas_StudentuInfoSistema.Database.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBEgzaminas_StudentuInfoSistema.Database.Repositories
 {
-    public class DepartmentRepository
+    public class DepartmentRepository : IDepartmentRepository
     {
+        private readonly StudentsContext _context;
+        private readonly DbSet<Department> _departments;
+
+        public DepartmentRepository(StudentsContext context)
+        {
+            _context = context;
+            _departments = _context.Set<Department>();
+        }
+
+        public Department GetById(string code)
+        {
+            return _departments.Find(code);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _departments;
+        }
+
+        public string Create(Department department)
+        {
+            _context.Add(department);
+            _context.SaveChanges();
+
+            return department.DepartmentCode;
+        }
+
+        public void Update(Department department)
+        {
+            _departments.Update(department);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int code)
+        {
+            var department = _departments.Find(code);
+            if (department != null)
+            {
+                _departments.Remove(department);
+                _context.SaveChanges();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
     }
 
     public class LectureRepository
--- DBEgzaminas_StudentuInfoSistema/Database/Repositories/Interfaces/IDepartmentRepository.cs
+++ DBEgzaminas_StudentuInfoSistema/Database/Repositories/Interfaces/IDepartmentRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace DBEgzaminas_StudentuInfoSistema.Database.Repositories.Interfaces
+﻿using DBEgzaminas_StudentuInfoSistema.Database.Entities;
+
+namespace DBEgzaminas_StudentuInfoSistema.Database.Repositories.Interfaces
 {
     public interface IDepartmentRepository
     {
+        Department GetById(string code);
+        IEnumerable<Department> GetAll();
+        string Create(Department department);
+        void Update(Department department);
+        void Delete(int code);
+        void SaveChanges();
+
     }
 
     public interface ILectureRepository
